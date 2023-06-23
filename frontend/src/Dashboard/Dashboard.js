37d96f54cb5298c7e9cfd9aca6eae74ffe1827eb
tiasaxena <tia.saxena041202@gmail.com>
2023-06-23 18:13:19 +0530

    creating dashboard layout

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+import './Dashborad.css';
+import logo from '../assets/logo.png';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='dashboard_container background_main_color'>
+      <div className='dashboard_left_section'>
+        <div className="dashboard_content_container">
+          Content
+        </div>
+        <div className="dashboard_rooms_container background_secondary_color">
+          Rooms
+        </div>
+      </div>
+      <div className="dashboard_right_section background_secondary_color">
+        <div className="dashboard_active_users_list">
+          Users
+        </div>
+        <div className='login-page_logo_container'>
+          <img className='login-page_logo_image' src={logo} alt='VideoTalkerApp'/>
+        </div>
+      </div>
+    </div>    
   )
 }
 
